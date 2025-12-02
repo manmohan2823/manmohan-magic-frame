d36ed25304500b9065835f471a7553d227aad794
Manmohan Kumar <64477882+manmohan2823@users.noreply.github.com>
2025-12-02 16:22:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  base: "/manmohan-magic-frame/", // :point_left: Add this
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
